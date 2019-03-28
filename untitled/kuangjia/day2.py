f16a70671f18ff7ed1488a738caf3d637af7b670
wangxianshen <2780125955@qq.com>
2019-03-28 10:01:29 +0800

    updata

--- untitled/kuangjia/day2.py
+++ untitled/kuangjia/day2.py
@@ -7,11 +7,14 @@ class TestdCaipu(unittest.TestCase):
         url="http://apis.juhe.cn/cook/query.php"
 
         par = {"key":"fb7fec936067e3dab6295891fd144e1e",
-               "menu":u"白菜"}
+               "menu":u"鸡蛋"}
+
 
         r=requests.post(url,params=par)
         print(r.status_code)
         print(r.text)
+
+
 
 if __name__=="__main__":
      unittest.main()
